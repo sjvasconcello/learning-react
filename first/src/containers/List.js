75a42156a08bf2dcbfe749edd22d1c418c931bd2
sjvasconcello <sjvasconcello@gmail.com>
2020-06-21 02:31:25 -0400

    :construction: Added data and improve List component
    Added Assets and media folder with data inside and List.js was improve

--- first/src/containers/List.js
+++ first/src/containers/List.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
-import Card from "../components/Card/Card"
+import Card from "../components/Card/Card";
 
 class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const movies = await fetch("../../assets/data.json");
+    const moviesJSON = await movies.json();
+
+    if (moviesJSON) {
+      this.setState({
+        data: moviesJSON,
+        loading: false,
+      });
+    }
+  }
+
   render() {
-    return <Card/>;
+    const { data, loading } = this.state;
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return data.map((movie) => (<Card key={movie.id} movie={movie}/>));
   }
 }
 
--- first/webpack.config.js
+++ first/webpack.config.js
@@ -9,11 +9,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ['babel-loader'],
       },
     ],
   },
