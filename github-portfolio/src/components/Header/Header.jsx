da5b23ea2f6a66c61d277359aab47bc4b925b992
sjvasconcello <sjvasconcello@gmail.com>
2020-06-22 02:13:37 -0400

    :construction: Github api with React
    Working components and github API - React Projects pag. 54

---
+++ github-portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "../Link/Link"
+import "./Header.css";
+
+const Header = ({ logo }) => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload
+    </p>
+    <Link url="https://reactjs.org" title="Learn React!" /> 
+  </header>
+);
+
+export default Header;
---
+++ github-portfolio/src/containers/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Profile from "./Profile"
+import Header from "../components/Header/Header"
+import logo from '../logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header logo={logo}/>
+        <Profile/>
+      </div>
+    );
+  }
+
+}
+
+export default App;
