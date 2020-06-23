3139ec046e24a16041cec7a858028cc4bb6c91ae
sjvasconcello <sjvasconcello@gmail.com>
2020-06-23 18:52:08 -0400

    :construction: Routing with React Router
    Declarative routing, routes with parameters and handling query - React Projects pag. 125

--- community-feed/src/containers/App.js
+++ community-feed/src/containers/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-import Header from '../components/Header/Header';
-import Feed from './Feed';
+import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Feed from "./Feed";
+import Question from "./Question";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -26,7 +28,13 @@ class App extends Component {
         <GlobalStyle />
         <AppWrapper>
           <Header />
-          <Feed />
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Feed} />
+              <Route path="/questions/:id" component={Question} />
+              <Route path="/questions" Component={Feed} />
+            </Switch>
+          </Router>
         </AppWrapper>
       </>
     );
--- community-feed/src/containers/Feed.js
+++ community-feed/src/containers/Feed.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Card from '../components/Card/Card';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Card from "../components/Card/Card";
+import { Link } from "react-router-dom";
+import queryString from "query-string";
 
 const FeedWrapper = styled.div`
   display: flex;
@@ -13,23 +15,57 @@ const Alert = styled.div`
   text-align: center;
 `;
 
-const ROOT_API = 'https://api.stackexchange.com/2.2/';
+const CardLink = styled(Link)`
+  text-decoration: none;
+  color: inherits;
+`;
 
+const PaginationBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PaginationLink = styled(Link)`
+  padding: 1%;
+  background: lightblue;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+`;
+
+const ROOT_API = "https://api.stackexchange.com/2.2/";
 class Feed extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const query = queryString.parse(props.location.search);
     this.state = {
       data: [],
+      page: query.page ? parseInt(query.page) : 1,
       loading: true,
-      error: '',
+      error: "",
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    const { page } = this.state;
+    this.fetchAPI(page)
+   
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.location.search !== this.props.location.search) {
+      const query = queryString.parse(this.props.location.search)
+      this.setState({page: parseInt(query.page)}, () =>
+        this.fetchAPI(this.state.page)
+      )
+    }
+  }
+  
+
+  async fetchAPI(page) {
     try {
-      const data = await fetch(
-        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`,
-      );
+      const data = await fetch(`${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow${page ? `&page=${page}` : ""}`);
       const dataJSON = await data.json();
 
       if (dataJSON) {
@@ -46,18 +82,32 @@ class Feed extends Component {
     }
   }
 
+
   render() {
-    const { data, loading, error } = this.state;
+    
+    const { data, page, loading, error } = this.state;
+    const { match } = this.props;
 
     if (loading || error) {
-      return <Alert>{loading ? 'Loading...' : error}</Alert>;
+      return <Alert>{loading ? "Loading..." : error}</Alert>;
     }
 
     return (
       <FeedWrapper>
-        {data.items.map(item => (
-          <Card key={item.question_id} data={item} />
+        {data.items.map((item) => (
+          <CardLink
+            key={item.question_id}
+            to={`/questions/${item.question_id}`}
+          >
+            <Card data={item} />
+          </CardLink>
         ))}
+        <PaginationBar>
+     
+    {page > 1 && <PaginationLink to={`${match.url}?page=${page - 1}`}>Previous</PaginationLink>}
+    {data.has_more && <PaginationLink to={`${match.url}?page=${page + 1}`}>Next</PaginationLink>}
+
+        </PaginationBar>
       </FeedWrapper>
     );
   }
